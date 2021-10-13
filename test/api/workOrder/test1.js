4493191065200093a83864098e6f4fb7ce6eb90c
taniyag-hexaware <TaniyaG@hexaware.com>
2021-10-13 12:11:28 +0530

    pulling

--- test/api/workOrder/test1.js
+++ test/api/workOrder/test1.js
@@ -28,10 +28,11 @@ describe('Tasks API', () => {
 
         describe("GET /api/workOrders", () => {
             it("It should GET all the tasks", (done) => {
-                chai.request(workOrder.getAllWorkOrders)
-                    .get("/workOrders/")
+                chai.request(workOrder)
+                    .get("/api/workOrders/")
                     .end((err, response) => {
                         response.should.have.status(200);
+                        res.body.should.have.keys(['_id','name','description']);
                        // response.body.should.be.a('string');
                         //response.body.length.should.be.eq(3);
                         //console.log(response.body);
